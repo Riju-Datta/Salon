fe082a29954972ca6beed15f6a41b084a5d5f592
Riju-Datta <dattariju052@gmail.com>
2026-09-10 17:48:41 +0600

    Fix production runtime startup

--- main.jsx
+++ main.jsx
@@ -1,9 +1,14 @@
-import "./site-enhancements.js";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./index.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const root = document.getElementById("root");
+
+if (!root) {
+  throw new Error("LUMÉ: root element was not found.");
+}
+
+ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
